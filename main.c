9de9704072a11099ed1175a6e252c59d9c926287
FirwoodLin <tan90_1697@163.com>
2023-10-02 21:27:06 +0800

    feat: basic functionality to forward client requests and server responses.

--- main.c
+++ main.c
@@ -7,23 +7,31 @@
 #include <string.h>
 #include <unistd.h>
 
-void handle_client(int fd);
+void client_to_mn(int, char **);
 
-int main(void)
-{
+void mn_to_client(int, const char *);
+
+void process_data(char **);
+
+void mn_to_server(int, const char *);
+
+void server_to_mn(int, char **);
+
+int main(void) {
+    // bind & listen; mn as a server
     struct sockaddr_in sa;
     memset(&sa, 0, sizeof sa);
     sa.sin_family = AF_INET;
     sa.sin_port = htons(1235);
     sa.sin_addr.s_addr = htonl(INADDR_ANY);
 
-    int server_fd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+    int server_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (server_fd == -1) {
         perror("cannot create socket");
         exit(1);
     }
 
-    if (bind(server_fd, (struct sockaddr *)&sa, sizeof sa) == -1) {
+    if (bind(server_fd, (struct sockaddr *) &sa, sizeof sa) == -1) {
         perror("bind failed");
         close(server_fd);
         return 1;
@@ -34,7 +42,7 @@ int main(void)
         close(server_fd);
         return 1;
     }
-
+    // accept data
     for (;;) {
         int client_fd = accept(server_fd, NULL, NULL);
 
@@ -43,9 +51,33 @@ int main(void)
             close(server_fd);
             return 1;
         }
-
-        handle_client(client_fd);
-
+        char *client_msg = NULL;    // data that comes from client
+        client_to_mn(client_fd, &client_msg);
+        process_data(&client_msg);
+        // new socket between mn and server; mn as a client
+        char proxy_pass_ip[100] = "127.0.0.1";
+        int proxy_pass_port = 7000;
+        int fd;
+        struct sockaddr_in dest_addr;
+        fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+        if (fd == -1) {
+            perror("socket");
+            exit(1);
+        }
+        dest_addr.sin_family = AF_INET;
+        dest_addr.sin_port = htons(proxy_pass_port);
+        dest_addr.sin_addr.s_addr = inet_addr(proxy_pass_ip);
+        if (connect(fd, (struct sockaddr *) &dest_addr, sizeof dest_addr) < 0) {
+            perror("connect");
+            exit(1);
+        }
+        // data process via mn_server socket
+        mn_to_server(fd, client_msg); // send data via new socket
+        char *server_msg;
+        server_to_mn(fd, &server_msg);// receive data from socket
+        // send data via old socket
+        mn_to_client(client_fd, client_msg);
+        printf("finished a client_fd");
         if (shutdown(client_fd, SHUT_RDWR) == -1) {
             perror("shutdown failed");
         }
@@ -56,20 +88,62 @@ int main(void)
     return 0;
 }
 
-void handle_client(int fd){
-    for(;;) {
-        printf("begin fd:%d\n",fd);
-        char buf[1024];
-        memset(buf, 0x00, sizeof(buf));
-        ssize_t ret = read(fd, buf, 1023);
-        if(ret == 0) {
-            return;
-        }
-        if(ret == -1) {
-            perror("read");
-            return;
-        }
-        printf("buf size:%zd",ret);
-        printf("%s\n", buf);
+void client_to_mn(int fd, char **received_data) {
+    // note: use char ** instead of char *
+    // TODO: what if the data pack is larger than 1024 byte?
+    // TODO: put multipart data together
+//    for(;;) {
+    printf("begin fd:%d\n", fd);
+    char buf[1024];
+    memset(buf, 0x00, sizeof(buf));
+    ssize_t ret = read(fd, buf, 1023);
+    *received_data = (char *) malloc(1024);
+    strcpy(*received_data, buf);
+    if (ret == 0) {
+        return;
+//            break;
     }
+    if (ret == -1) {
+        perror("read");
+        return;
+    }
+    printf("buf size:%zd\n", ret);
+    printf("%s\n", buf);
+//    }
+}
+
+void mn_to_server(int fd, const char *data) {
+
+
+    // 发送数据
+    ssize_t num_bytes_sent = send(fd, data, strlen(data), 0);
+    if (num_bytes_sent == -1) {
+        perror("sendto");
+        exit(1);
+    }
+    printf("Sent %zd bytes to the server.\n", num_bytes_sent);
+}
+
+void mn_to_client(int fd, const char *data) {
+    printf("mn_to_client;data to write:\n%s\n", data);
+    ssize_t ret;
+    ret = write(fd, data, strlen(data));
+    if (ret < 0) {
+        printf("mn_to_client;write error\n");
+    }
+}
+
+void process_data(char **data) {
+    // TODO: replace the host
+}
+
+void server_to_mn(int fd, char **data) {
+    char buff[1024];
+    memset(buff, 0x00, sizeof buff);
+    if (recv(fd, buff, 1024, 0) < 0) {
+        printf("Error while receiving server's msg\n");
+        exit(-1);
+    }
+    *data = (char *) malloc(strlen(buff));
+    strcpy(*data, buff);
 }
